eb9d49fc6237b3b769b79f45002552b76bf165f5
chandan916 <sainichandan512@gmail.com>
2020-03-25 19:17:11 +0530

    changed database

--- include/check_if_added.php
+++ include/check_if_added.php
@@ -2,7 +2,8 @@
   /*function to check if item is added to cart or not */
   function check_if_added_to_cart($num)
 	{
-    	$con=mysqli_connect("localhost","root","","store");
+    	$con=mysqli_connect("ec2-23-20-129-146.compute-1.amazonaws.com","qmihdzhyolwyau","6e3493f5e5a03cadef0d1b349ea41da70fe359f681353a7c5185b18b6413b2c7
+		","store");
 	if(isset($_SESSION['user_id']))
 	{
 	$user_id=$_SESSION['user_id'];
--- include/common.php
+++ include/common.php
@@ -1,4 +1,4 @@
 <?php 
-  $con=mysqli_connect("localhost","root","","store");
+  $con=mysqli_connect("ec2-23-20-129-146.compute-1.amazonaws.com","qmihdzhyolwyau","6e3493f5e5a03cadef0d1b349ea41da70fe359f681353a7c5185b18b6413b2c7","store");
   session_start();
 ?>
